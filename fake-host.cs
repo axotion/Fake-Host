bcf80ca98dfb946c60c48cff0ff648077ebc5794
axotion <axotion@linux.pl>
2015-04-17 12:19:21 +0200

    Update fake-host.cs
    If file exist, struct instead of class, minimum 2 arguments.

--- fake-host.cs
+++ fake-host.cs
@@ -5,33 +5,34 @@ using System.Security.Principal;
 /* For example
  * 127.0.0.1 google.com
  * Where 127.0.0.1 is your server/whatever
- * Also yo can run it with parametr xxx.xxx.xxx.xxx www.something.com
+ * Also you can run it with parametr like xxx.xxx.xxx.xxx www.something.com
  * */
 
-class Var{
+struct Var{
 public static string IP = "127.0.0.1";
 public static string Dest = "www.google.com";
 };
-
-		class HostChanger
+	class HostChanger
 	{
 	public static void Main (string[] args)
 	{
-		if (args.Length > 0) {
+	    if (args.Length > 1) {
 			Var.IP = args [0];
 			Var.Dest = args[1];
-		}
+		                         }
 			bool isAdmin;
 			WindowsIdentity identity = WindowsIdentity.GetCurrent ();
 			WindowsPrincipal principal = new WindowsPrincipal (identity);
 			isAdmin = principal.IsInRole (WindowsBuiltInRole.Administrator);
-			if (isAdmin != false) {
-
+		    if(File.Exists("c:\\windows\\system32\\drivers\\etc\\hosts")){
+		    if (isAdmin != false) {
 			File.SetAttributes ("c:\\windows\\system32\\drivers\\etc\\hosts", FileAttributes.Normal);
 			File.AppendAllText ("c:\\windows\\system32\\drivers\\etc\\hosts", Environment.NewLine + Var.IP+" "+Var.Dest+Environment.NewLine);
 			File.AppendAllText ("c:\\windows\\system32\\drivers\\etc\\hosts", Var.IP+" "+Var.Dest.Remove (0,4)+Environment.NewLine);
 				} else {
 				Console.WriteLine ("You must run it as admin");
+			    Console.ReadKey ();
+			           }
+		        }
 		}
-	}
 }
